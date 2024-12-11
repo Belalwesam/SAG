80b78783f2457b71c7c44a90c02bfa186a1a1e5b
Belalwesam <belal.ahmaf@gmail.com>
2024-12-11 20:21:07 +0300

    almost done

--- app/Http/Controllers/Client/TicketController.php
+++ app/Http/Controllers/Client/TicketController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Client;
 use App\Models\Ticket;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
 use App\Http\Requests\Client\TicketSubmitRequest;
 
 class TicketController extends Controller
@@ -23,21 +24,34 @@ class TicketController extends Controller
     public function store(TicketSubmitRequest $request)
     {
         $ticket_id = uniqid();
-        // $ticket = auth()->user()->tickets()->create([
-        //     "subject" => $request->subject,
-        //     "project_id" => $request->project_id,
-        //     "description" => $request->description,
-        //     "priority" => $request->priority,
-        //     "ticket_id" => $ticket_id
-        // ]);
+        $ticket = auth()->user()->tickets()->create([
+            "subject" => $request->subject,
+            "project_id" => $request->project_id,
+            "description" => $request->description,
+            "priority" => $request->priority,
+            "ticket_id" => $ticket_id
+        ]);
 
         if ($request->files && count($request->files) > 0) {
             foreach ($request->files as $file) {
                 $file_type = '';
                 $mime_type = $file[0]->getMimeType();
                 if (str_starts_with($mime_type, 'image/')) {
-                    # code...
+                    $file_type = 'image';
                 }
+                if (str_starts_with($mime_type, 'video/')) {
+                    $file_type = 'video';
+                }
+                if (str_starts_with($mime_type, 'application/pdf')) {
+                    $file_type = 'pdf';
+                }
+
+                $storage_path = 'tickets/' . $ticket->ticket_id;
+                $path = Storage::putFileAs($storage_path, $file[0], $file[0]->getClientOriginalName());
+                $ticket->files()->create([
+                    "type" => $file_type,
+                    "path" => $path
+                ]);
             }
         }
 
