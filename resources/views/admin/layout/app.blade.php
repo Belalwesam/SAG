0fdbbfa98d0d9fae418c99da0ad75276a4cf9a4c
Belalwesam <belal.ahmaf@gmail.com>
2024-12-23 19:34:10 +0300

    the changes

--- resources/views/admin/layout/app.blade.php
+++ resources/views/admin/layout/app.blade.php
@@ -9,7 +9,7 @@
     <meta name="viewport"
         content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-    <title>Frest | @yield('title')</title>
+    <title>SAG | @yield('title')</title>
 
     <meta name="description" content="" />
 
