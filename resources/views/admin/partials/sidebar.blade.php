f93c3cf4976cb0e3c782f11ce3240165b207fe78
Belalwesam <belal.ahmaf@gmail.com>
2024-12-23 19:37:41 +0300

    the arrows

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -9,8 +9,8 @@
          </a>
 
          <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto">
-             <i class="bx menu-toggle-icon d-none d-xl-block fs-4 align-middle"></i>
-             <i class="bx bx-x d-block d-xl-none bx-sm align-middle"></i>
+             <i class="bx bx-left-arrow-alt d-none d-xl-block fs-4 align-middle"></i>
+             {{-- <i class="bx bx-x d-block d-xl-none bx-sm align-middle"></i> --}}
          </a>
      </div>
 
