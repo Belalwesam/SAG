c874c8b28155e078d9cb6b79b25426223e7c8ea0
Belalwesam <belal.ahmaf@gmail.com>
2024-12-25 15:05:17 +0300

    added the filteration for the tickets table in admin's index

--- app/Http/Controllers/Admin/TicketController.php
+++ app/Http/Controllers/Admin/TicketController.php
@@ -27,7 +27,16 @@ class TicketController extends Controller
         }
         $data = $data->when($request->status, function ($query) use ($request) {
             return $query->where('status', $request->status);
-        });
+        })
+            ->when($request->date_from, function ($query) use ($request) {
+                $query->where('created_at', '>=', $request->date_from);
+            })
+            ->when($request->date_to, function ($query) use ($request) {
+                $query->where('created_at', '<=', $request->date_to);
+            })
+            ->when($request->date_from && $request->date_to, function ($query) use ($request) {
+                $query->whereBetween('created_at', [$request->date_from, $request->date_to]);
+            });
         $data = $data->get();
         return DataTables::of($data)
             ->addIndexColumn()
