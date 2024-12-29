5339493aa000fac64ccd1acb0e2959c655b0adb7
Belalwesam <belal.ahmaf@gmail.com>
2024-12-29 19:42:57 +0300

    added he clients list

---
+++ app/Http/Controllers/Client/ProjectController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\Client;
+
+use App\Models\User;
+use App\Models\Project;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Yajra\DataTables\Facades\DataTables;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+        return view('client.pages.projects.index');
+    }
+
+    public function getProjectsList(Request $request)
+    {
+        $data = auth()->user()->projects()->latest();
+        $data = $data->when($request->date_from, function ($query) use ($request) {
+            $query->where('created_at', '>=', $request->date_from);
+        })
+            ->when($request->date_to, function ($query) use ($request) {
+                $query->where('created_at', '<=', $request->date_to);
+            })
+            ->when($request->date_from && $request->date_to, function ($query) use ($request) {
+                $query->whereBetween('created_at', [$request->date_from, $request->date_to]);
+            });
+        $data = $data->get();
+        return DataTables::of($data)
+            ->addIndexColumn()
+            ->editColumn('created_at', function ($row) {
+                return $row->created_at->format('d-m-Y');
+            })
+            ->editColumn('user_id', function ($row) {
+                return $row->user->name;
+            })
+            ->addColumn('actions', function ($row) {
+                $edit_text = trans('general.edit');
+                $delete_text = trans('general.delete');
+                $btns = <<<HTML
+                    <div class="dropdown d-flex justify-content-center">
+                        <button type="button" class="btn dropdown-toggle hide-arrow p-0" data-bs-toggle="dropdown" aria-expanded="false">
+                          <i class="bx bx-dots-vertical-rounded"></i>
+                        </button>
+                        <ul class="dropdown-menu dropdown-menu-end">
+                            <li><a class="dropdown-item edit-btn"
+                             data-id="{$row->id}"
+                             data-name = "{$row->name}"
+                             data-hours = "{$row->hours}"
+                             data-user = "{$row->user_id}"
+                              data-bs-toggle="modal"
+                              data-bs-target = "#editProjectModal"
+                              href="javascript:void(0);"><i class="bx bx-edit me-0 me-2 text-primary"></i>{$edit_text}</a></li>
+                             <li>
+                              <a class="dropdown-item delete-btn"
+                                data-id = "{$row->id}"
+                              href="javascript:void(0);"><i class="bx bx-trash me-0 me-2 text-danger"></i>{$delete_text}</a></li>
+                          </ul>
+                        </div>
+                HTML;
+
+                if (auth('admin')->user()->hasAbilityTo('edit projects')) {
+                    return $btns;
+                }
+                return;
+            })
+            ->rawColumns(['actions', 'user_id'])
+            ->make(true);
+    }
+}
